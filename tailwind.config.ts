b8e39903e42687be74273d6ffce0897486f3511e
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-01-03 18:54:36 +0000

    define basic color palette in tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      brand: '#ff5f00',
+      primary: '#1a1a1a',
+      secondary: '#828287',
+      tertiary: '#c9c9cf'
     },
   },
   plugins: [],

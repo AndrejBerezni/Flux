bfc1dad59835f5329b2230879619df08674ea671
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-03-01 14:14:21 +0000

    delete layout for subscriptions - keep everything on page

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -1,5 +1,13 @@
+import BenefitsSection from '@/components/subscriptions/BenefitsSection'
+import SubscriptionsCarousel from '@/components/subscriptions/SubscriptionsCarousel'
 import SubscriptionSelectionSection from '@/components/subscriptions/SubscriptionSelectionSection'
 
-export default function SubscriptionsSelectionPage() {
-  return <SubscriptionSelectionSection />
+export default function SubscriptionsPageLayout() {
+  return (
+    <main className="flex flex-1 flex-col">
+      <SubscriptionsCarousel />
+      <BenefitsSection />
+      <SubscriptionSelectionSection />
+    </main>
+  )
 }

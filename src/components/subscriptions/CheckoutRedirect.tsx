3d42b4e22d0e5d1be4c5b6dae1ce87b278dc3441
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-03-03 11:29:11 +0000

    demand user to select vehicle type before proceeding with subscription checkout

--- src/components/subscriptions/CheckoutRedirect.tsx
+++ src/components/subscriptions/CheckoutRedirect.tsx
@@ -2,24 +2,65 @@
 import { useState } from 'react'
 
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useSelector, useDispatch } from 'react-redux'
+import { setMessage } from '@/store/modal'
 
-import { getAuthStatus } from '@/store/authentication/selectors'
+import { getAuthStatus, getUserId } from '@/store/authentication/selectors'
 import { showModal } from '@/store/modal'
 
-export default function CheckoutRedirect({ subId }: { subId: string }) {
+export default function CheckoutRedirect({
+  subId,
+  subStripeId,
+  subName,
+  subPeriod,
+}: {
+  subId: string
+  subStripeId: string
+  subName: string
+  subPeriod: string
+}) {
   const dispatch = useDispatch()
   const isAuth = useSelector(getAuthStatus)
+  const userId = useSelector(getUserId)
   const router = useRouter()
+  const searchParams = useSearchParams()
   const [agreedToTerms, setAgreedToTerms] = useState<boolean>(false)
 
   const handleCheckBox = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAgreedToTerms(event.target.checked)
   }
 
+  const handleVehicleInformation = () => {
+    const selectedVehicle = searchParams.get('vehicleType')
+    if (!selectedVehicle) {
+      dispatch(
+        setMessage({
+          type: 'error',
+          text: 'Please select vehicle type for this subscription.',
+        })
+      )
+      return null
+    }
+    return selectedVehicle
+  }
+
   const handleCheckout = async () => {
-    const response = await fetch(`/api/subscriptions/checkout?subId=${subId}`)
+    let selectedVehicle
+    if (subName !== 'Platinum') {
+      selectedVehicle = handleVehicleInformation()
+      if (!selectedVehicle) {
+        return
+      }
+    }
+    // const requestData = {
+    //   subId,
+    //   subStripeId,
+    //   userId,
+    // }
+    const response = await fetch(
+      `/api/subscriptions/checkout?subId=${subId}&subStripeId=${subStripeId}&userId=${userId}`
+    )
     const url = await response.json()
     if (url) {
       router.push(url)
